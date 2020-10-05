d40c51bd6667b00a36d6d3fb21707a444fb966c9
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2020-10-05 20:16:56 +0530

    Refactored Code

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 from google.cloud import speech
 from time import strftime
 from time import gmtime
-def transcribe_model_selection_gcs(gcs_uri, model, filename):
+from google.cloud import storage
+import wave, os, glob
+import subprocess
+
+BUCKET_NAME = 'testdata1234'
+
+"""
+    Upload Local File to Cloud Storage Bucket
+
+    :param blob_name: Blob FileName
+    :param path_to_file: Local path to file
+    :return: File path to this resource in Cloud Storage
+"""
+def upload_to_bucket(blob_name, path_to_file):
+    print(f"Uploading {blob_name}")
+    storage_client = storage.Client()
+
+    bucket = storage_client.get_bucket(BUCKET_NAME)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_filename(path_to_file)
+    return f'gs://{BUCKET_NAME}/{blob_name}'
+
+
+"""
+    Speech to Text
+
+    :param gcs_uri: Audio File's Cloud Storage URL
+    :param model: Speech to Text Transcription models
+    :return: Speech to Text Response
+"""
+def speech_to_text_conversion(gcs_uri, model):
+    print(f"Speech to Text Conversion Started...")
     client = speech.SpeechClient()
 
     audio = speech.RecognitionAudio(uri=gcs_uri)
@@ -19,8 +50,19 @@ def transcribe_model_selection_gcs(gcs_uri, model, filename):
 
     print("Waiting for operation to complete...")
     response = operation.result(timeout=1000000)
+    return response
+
+"""
+    Generate Subtitile and Save Locally
+
+    :param speech_to_text_response: Speech to Text Response
+    :param filename: Video File BaseName
+    :return: void
+"""
+def generate_subtitle(speech_to_text_response, file_basename):
+    print(f"Subtitle Generate Started...")
     res = []
-    for i, result in enumerate(response.results):
+    for i, result in enumerate(speech_to_text_response.results):
         for idx in range(len(result.alternatives)):
             if (len(res) <= idx):
                 res.append('')
@@ -44,28 +86,24 @@ def transcribe_model_selection_gcs(gcs_uri, model, filename):
             res[idx] += f"{strftime('%H:%M:%S', gmtime(sentence_start_time))},{sentence_start_time_ms} --> {strftime('%H:%M:%S', gmtime(sentence_end_time))},{sentence_end_time_ms}\n"
             res[idx] += f"{alternative.transcript}\n\n"
     for i in range(len(res)):
-        f = open(f"{filename}-subtitle-{i}.srt", "w")
+        f = open(f"{file_basename}-subtitle-{i}.srt", "w")
         f.write(res[i])
         f.close()
 
-from google.cloud import storage
-bucket_name = 'testdata1234'
+# Main
+for video_filename in glob.glob(os.path.join('', '*.mp4')):
+    audio_file_basename = '.'.join(video_filename.split('.')[0:-1])
+    audio_filename = f"{audio_file_basename}.wav"
 
-def upload_to_bucket(blob_name, path_to_file):
-    storage_client = storage.Client()
+    # Converting MP4 to WAV
+    print(f"\nConverting {video_filename} to {audio_filename}")
+    subprocess.Popen(f"ffmpeg -i \"{video_filename}\" -acodec pcm_s16le -ac 1 -ar 16000 -y \"{audio_filename}\"", stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    
+    # Upload Converted WAV File to Cloud Bucket
+    gcs_uri = upload_to_bucket(audio_filename, audio_filename)
+    
+    # Speech to Text Conversion
+    speech_to_text_response = speech_to_text_conversion(gcs_uri, "default")
 
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(blob_name)
-    blob.upload_from_filename(path_to_file)
-    return blob.public_url
-
-import wave, os, glob
-import subprocess
-for filename in glob.glob(os.path.join('', '*.mp4')):
-    audio_filename = f"{'.'.join(filename.split('.')[0:-1])}.wav"
-    print(f"\nConverting {filename} to {audio_filename}")
-    subprocess.Popen(f"ffmpeg -i \"{filename}\" -acodec pcm_s16le -ac 1 -ar 16000 \"{audio_filename}\"", stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-    print(f"Uploading {audio_filename}")
-    upload_to_bucket(audio_filename, audio_filename)
-    print(f"Subtitle Generate Started...")
-    transcribe_model_selection_gcs(f"gs://{bucket_name}/{audio_filename}", "default", '.'.join(filename.split('.')[0:-1]))
+    # Generate Subtitile
+    generate_subtitle(speech_to_text_response, audio_file_basename)
